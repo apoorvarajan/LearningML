5220aa266210c83cd7074c22ba1789f76501c3d0
ApoorvaRajan <apoorvarajan1997@gmail.com>
2021-06-13 23:07:18 +0530

    SGDRegressor

--- Ecommerce.py
+++ Ecommerce.py
@@ -8,10 +8,10 @@ y=reduced_data['Yearly Amount Spent']
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=101)
 from sklearn.linear_model import LinearRegression
-model1 = LinearRegression()
-model1.fit(X_train,y_train)
-pred1 = model1.predict(X_test)
+model = LinearRegression()
+model.fit(X_train,y_train)
+pred = model.predict(X_test)
 from sklearn import metrics
-print("mean absolute error = ", metrics.mean_absolute_error(y_test,pred1))
-print("mean squared error = ", metrics.mean_squared_error(y_test,pred1))
-print("root mean squared error = ", np.sqrt(metrics.mean_squared_error(y_test,pred1)))
+print("mean absolute error = ", metrics.mean_absolute_error(y_test,pred))
+print("mean squared error = ", metrics.mean_squared_error(y_test,pred))
+print("root mean squared error = ", np.sqrt(metrics.mean_squared_error(y_test,pred)))
--- USA_Housing_Project_SGDRegressor.py
+++ USA_Housing_Project_SGDRegressor.py
@@ -1,19 +1,20 @@
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 df = pd.read_csv('USA_Housing.csv')
-df.head()
 df.corr().Price.sort_values()
 X=df[['Avg. Area Income','Avg. Area House Age','Area Population','Avg. Area Number of Rooms','Avg. Area Number of Bedrooms']]
 y=df.Price
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+X2 = X
+scaler.fit(X2)
+scaled_X2 =scaler.transform(X2)
 from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=101)
-from sklearn.linear_model import LinearRegression
-model = LinearRegression()
+X_train,X_test,y_train,y_test=train_test_split(scaled_X2,y,test_size=0.3,random_state=101)
+from sklearn.linear_model import SGDRegressor
+model = SGDRegressor()
 model.fit(X_train,y_train)
 pred = model.predict(X_test)
-plt.scatter(pred,y_test)
 from sklearn import metrics
 print("mean absolute error = ", metrics.mean_absolute_error(y_test,pred))
 print("mean squared error = ", metrics.mean_squared_error(y_test,pred))
